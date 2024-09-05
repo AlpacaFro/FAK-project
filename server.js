3ee3ea10cd27872116ec4d6a4dbd065d6fe1aedc
Amit Kubani <amitkubani@gmail.com>
2024-09-05 13:40:22 +0300

    commit

--- server.js
+++ server.js
@@ -14,17 +14,36 @@ app.addProduct = function (title, price, description, image, category){
                 category: category
             }
         )
-    })
+        })//.then(response => response.json())
         // .then(res=>res.json())
         // .then(json=>{
         //     console.log(json)
         // })
 }
 app.add = function() {return 'success!'};
+/**Acceptance Criteria:
 
+The product detail page should display the product’s name, full description, price, category, and images.
+
+Product reviews and ratings should be displayed if available.
+
+Users should see available stock levels.
+
+Include an "Add to Cart" button on the product detail page. */
+app.get('/products/:id',(req, res) => {
+    const id = req.params.id;
+    fetch(`https://fakestoreapi.com/products/${id}`)
+            .then(response=>{
+                return response.json();
+            })
+            .then(body =>{
+                console.log(body);
+                res.send(body);
+            })
+});
 //app.addProduct('p1',3000,"some product",'https://i.pravatar.cc',"general").then(result => console.log(result));
-// app.listen(port, ()=>{
-//     console.log(`server is listening on port ${port}`);
-// });
+app.listen(port, ()=>{
+    console.log(`server is listening on port ${port}`);
+});
 
 module.exports = app;
