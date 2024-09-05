59ae8c690d6eb07089a1f447cd4f925dbf01ef69
Amit Kubani <amitkubani@gmail.com>
2024-09-05 10:52:39 +0300

    commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+
+function addProduct(title, price, description, image, category){
+    fetch('https://fakestoreapi.com/products',{
+        method:"POST",
+        body:JSON.stringify(
+            {
+                title: title,
+                price: price,
+                description: description,
+                image: image,
+                category: category
+            }
+        )
+    })
+        .then(res=>res.json())
+        .then(json=>console.log(json))
+}
+
+addProduct('p1',3000,"some product",'https://i.pravatar.cc',"general");
