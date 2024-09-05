ff1d73f05b0c7e667f64a2f778b461f489a867b2
Amit Kubani <amitkubani@gmail.com>
2024-09-05 12:27:45 +0300

    commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-
+const port = 3000;
 
 app.addProduct = function (title, price, description, image, category){
-    fetch('https://fakestoreapi.com/products',{
+    return fetch('https://fakestoreapi.com/products',{
         method:"POST",
         body:JSON.stringify(
             {
@@ -15,11 +15,16 @@ app.addProduct = function (title, price, description, image, category){
             }
         )
     })
-        .then(res=>res.json())
-        .then(json=>{
-            console.log(json)
-            return json;
-        })
+        // .then(res=>res.json())
+        // .then(json=>{
+        //     console.log(json)
+        // })
 }
+app.add = function() {return 'success!'};
 
-//addProduct('p1',3000,"some product",'https://i.pravatar.cc',"general");
+//app.addProduct('p1',3000,"some product",'https://i.pravatar.cc',"general").then(result => console.log(result));
+// app.listen(port, ()=>{
+//     console.log(`server is listening on port ${port}`);
+// });
+
+module.exports = app;
--- tests/app.test.js
+++ tests/app.test.js
@@ -3,8 +3,21 @@ const app = require('../server.js');
 
 describe('addProduct', () => {
     it('should respond with JSON data', async () => {
-        const response = await app.addProduct();
-        expect(response.statusCode).toBe(200);
+        const response = await app.addProduct('p1',3000,"some product",'https://i.pravatar.cc',"general");
+        expect(response.status).toBe(200);
         expect(response.body).toBeDefined();
     });
-})
+});
+
+describe('add', () => {
+    it('should respond with "success!"', ()=>{
+        const response = app.add();
+        expect(response).toBe("success!");
+    });
+});
+
+// describe('add', () => {
+//     test('return success', () => {
+//         expect(app.add()).toBe('success!');
+//     });
+// });
