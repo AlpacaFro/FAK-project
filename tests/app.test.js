ee87a395a6c578fcfacbe86ad96296fc06c51e02
Amit Kubani <amitkubani@gmail.com>
2024-09-06 19:27:08 +0300

    commit

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../server.js');
+const app = require('../server/server.js');
 
 describe('addProduct', () => {
     it('should respond with JSON data', async () => {
