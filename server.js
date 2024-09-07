767a85baa3358fb69beda74d400b50d26d476ad1
agit <box999box@gmail.com>
2024-09-07 21:14:22 +0300

    commit

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 3000;
+
+
+//app.getProductDetails('https://fakestoreapi.com/products/1');
+app.getProductDetails = async (url) => {
+    try {
+        const response = await axios.get(url);
+        console.log(response.data);
+        
+        return response;
+    } catch (error) {
+        console.log(error.response.status);
+        return error.response;
+    }
+};
 
 //app.addProduct('p1',3000,"some product",'https://i.pravatar.cc',"general").then(result => console.log(result));
 app.addProduct = function (title, price, description, image, category){
@@ -17,31 +32,7 @@ app.addProduct = function (title, price, description, image, category){
         )
     })
 }
-
-app.get('/products/:id',(req, res) => {
-    const id = req.params.id;    
-    fetch(`https://fakestoreapi.com/products/${id}`)
-            .then(response=>{
-                return response.json();
-            })
-            .then(body =>{
-                console.log(body);
-                res.send(body);
-            })
-});
-
-app.get('/products',(req, res) => {
-    const id = req.params.id;    
-    fetch(`https://fakestoreapi.com/products`)
-            .then(response=>{
-                return response.json();
-            })
-            .then(body =>{
-                console.log(body);
-                res.send(body);
-            })
-});
-
+  
 app.listen(port, ()=>{
     console.log(`server is listening on port ${port}`);
 });
--- tests/app.test.js
+++ tests/app.test.js
@@ -16,3 +16,51 @@ describe('GET /products/:id', () => {
         expect(response._body.id).toBeDefined();
     })
 });
+
+describe('getDetails', () => {
+    test('details', async () => {
+      const response = await app.getProductDetails(
+        'https://fakestoreapi.com/products/1'
+      );
+      expect(response.data.id).toBe(1);
+    });
+  });
+  
+
+// test status code 200
+describe(' status code 200', () => {
+    test('test if status code is 200', async () => {
+      const response = await app.getProductDetails(
+        'https://fakestoreapi.com/products/1'
+      );
+      expect(response.status).toBe(200);
+    });
+  });
+  
+  // test status code 404
+describe('check status code 404', () => {
+    test('test if bad route give send code 404', async () => {
+      const response = await app.getProductDetails(
+        'https://fakestoreapi.com/procts!@#$$%^^&&()(*&^%$#@!'
+      );
+      expect(response.status).toBe(404);
+    });
+  });
+  
+  describe('check the right price', () => {
+    test('test if item 16 have the right price from the API', async () => {
+      const response = await app.getProductDetails(
+        'https://fakestoreapi.com/products/16'
+      );
+      expect(response.data.price).toBe(29.95);
+    });
+  });
+  
+  describe('check the right rating', () => {
+    test('test if item 16 have the right rating from the API', async () => {
+      const response = await app.getProductDetails(
+        'https://fakestoreapi.com/products/16'
+      );
+      expect(response.data.rating.rate).toBe(2.9);
+    });
+  });
