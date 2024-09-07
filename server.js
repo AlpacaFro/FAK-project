49f6909a9c0e061bf54382c528ea8c7504abbde9
Amit Kubani <160615422+AgitAgit@users.noreply.github.com>
2024-09-07 20:18:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//app.addProduct('p1',3000,"some product",'https://i.pravatar.cc',"general").then(result => console.log(result));
 app.addProduct = function (title, price, description, image, category){
     return fetch('https://fakestoreapi.com/products',{
         method:"POST",
@@ -14,26 +15,11 @@ app.addProduct = function (title, price, description, image, category){
                 category: category
             }
         )
-        })//.then(response => response.json())
-        // .then(res=>res.json())
-        // .then(json=>{
-        //     console.log(json)
-        // })
+        })
 }
-app.add = function() {return 'success!'};
-/**Acceptance Criteria:
-
-The product detail page should display the product’s name, full description, price, category, and images.
-
-Product reviews and ratings should be displayed if available.
-
-Users should see available stock levels.
-
-Include an "Add to Cart" button on the product detail page. */
 
 app.get('/products/:id',(req, res) => {
-    const id = req.params.id;
-    
+    const id = req.params.id;    
     fetch(`https://fakestoreapi.com/products/${id}`)
             .then(response=>{
                 return response.json();
@@ -44,7 +30,6 @@ app.get('/products/:id',(req, res) => {
             })
 });
 
-//app.addProduct('p1',3000,"some product",'https://i.pravatar.cc',"general").then(result => console.log(result));
 app.listen(port, ()=>{
     console.log(`server is listening on port ${port}`);
 });
